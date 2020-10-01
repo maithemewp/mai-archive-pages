edba8810b1d63c153245782a7ce4174c5605ec24
Mike Hemberger <mike@thestizmedia.com>
2020-10-01 16:45:52 -0400

    Initial commit

---
+++ includes/archive-page.php
@@ -0,0 +1,330 @@
+<?php
+
+add_action( 'init', function() {
+	new Mai_Archive_Pages;
+});
+
+class Mai_Archive_Pages {
+
+	/**
+	 * Post type name.
+	 *
+	 * @var string
+	 */
+	public $post_type;
+
+	/**
+	 * Slug prefix.
+	 *
+	 * @var string
+	 */
+	public $prefix;
+
+	/**
+	 * If archive is a post type.
+	 *
+	 * @var bool
+	 */
+	public $is_post_type;
+
+	/**
+	 * If archive is a taxonomy.
+	 *
+	 * @var bool
+	 */
+	public $is_taxonomy;
+
+	/**
+	 * Class constructor
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		if ( ! function_exists( 'genesis' ) ) {
+			return;
+		}
+
+		$this->post_type = 'mai_archive_page';
+		$this->prefix    = 'maiap';
+
+		add_action( 'template_redirect',    [ $this, 'new_archive_page' ] );
+		add_action( 'admin_bar_menu',       [ $this, 'admin_bar_link_front' ], 90 );
+		add_action( 'admin_bar_menu',       [ $this, 'admin_bar_link_back' ], 90 );
+		add_action( 'genesis_before_while', [ $this, 'do_content' ] );
+	}
+
+	function new_archive_page() {
+		if ( ! $this->has_access() ) {
+			return;
+		}
+
+		if ( 'new' !== filter_input( INPUT_GET, 'maiap', FILTER_SANITIZE_STRING ) ) {
+			return;
+		}
+
+		$title = filter_input( INPUT_GET, 'maiap_title', FILTER_SANITIZE_STRING );
+		$slug  = filter_input( INPUT_GET, 'maiap_slug', FILTER_SANITIZE_STRING );
+
+		if ( ! ( $title && $slug ) ) {
+			return;
+		}
+
+		$existing = get_page_by_path( $slug, OBJECT, $this->post_type );
+
+		if ( $existing ) {
+			wp_redirect( get_edit_post_link( $existing ) );
+			exit();
+		}
+
+		$new_id = wp_insert_post( [
+			'post_type'      => $this->post_type,
+			'post_title'     => urldecode( $title ),
+			'post_name'      => $slug,
+			'post_status'    => 'draft',
+			'comment_status' => 'closed',
+			'ping_status'    => 'closed',
+		] );
+
+		if ( $new_id ) {
+			wp_redirect( get_edit_post_link( $new_id, false ) );
+			exit();
+		}
+
+		$clean_url = home_url( add_query_arg( [] ) );
+
+		wp_redirect( $clean_url );
+		exit();
+	}
+
+	function admin_bar_link_front( $wp_admin_bar ) {
+		if ( is_admin() ) {
+			return;
+		}
+
+		if ( ! $this->has_access() ) {
+			return;
+		}
+
+		$page_id = $this->get_page_id();
+
+		if ( $page_id ) {
+
+			$wp_admin_bar->add_node( [
+				'id'    => 'mai-archive-page',
+				'title' => '<span class="ab-icon dashicons dashicons-edit" style="margin-top: 4px;"></span><span class="ab-label">' . __( 'Edit Archive Content', 'mai-archive-pages' ) . '</span>',
+				'href'  => get_edit_post_link( $page_id ),
+			] );
+
+			return;
+		}
+
+		$title = $slug = '';
+
+		if ( $this->is_taxonomy() ) {
+
+			$term_id = get_queried_object_id();
+
+			if ( ! $term_id ) {
+				return;
+			}
+
+			$title = $this->get_term_title( $term_id );
+			$slug  = $this->get_term_slug( $term_id );
+
+		} elseif ( $this->is_post_type() ) {
+
+			$post_type = get_post_type();
+
+			if ( ! $post_type ) {
+				return;
+			}
+
+			$title = $this->get_post_type_title( $post_type );
+			$slug  = $this->get_post_type_slug( $post_type );
+		}
+
+		$url = home_url( add_query_arg( [
+			'maiap'       => 'new',
+			'maiap_title' => urlencode( $this->get_post_type_title( $post_type ) ),
+			'maiap_slug'  => $this->get_post_type_slug( $post_type ),
+		] ) );
+
+		if ( ! $url ) {
+			return;
+		}
+
+		$wp_admin_bar->add_node( [
+			'id'    => 'mai-archive-page',
+			'title' => '<span class="ab-icon dashicons dashicons-plus" style="margin-top: 4px;"></span><span class="ab-label">' . __( 'Add Archive Content', 'mai-archive-pages' ) . '</span>',
+			'href'  => $url,
+		] );
+	}
+
+	function admin_bar_link_back( $wp_admin_bar ) {
+		if ( ! is_admin() ) {
+			return;
+		}
+
+		if ( ! $this->has_access() ) {
+			return;
+		}
+
+		$screen = get_current_screen();
+
+		if ( empty( $screen->id ) || $this->post_type !== $screen->id ) {
+			return;
+		}
+
+		$post_id = filter_input( INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT );
+		$slug    = get_post_field( 'post_name', $post_id );
+		$parts   = explode( '_', $slug );
+		$prefix  = isset( $parts[0] ) ? $parts[0] : '';
+		$name    = isset( $parts[1] ) ? $parts[1] : '';
+		$id      = isset( $parts[2] ) ? $parts[2] : '';
+		$link    = false;
+
+		if ( $name && $id && taxonomy_exists( $name ) ) {
+			$link = get_term_link( (int) $id, $name );
+		}
+		elseif ( $name && post_type_exists( $name ) ) {
+			$link = get_post_type_archive_link( $name );
+		}
+
+		if ( ! $link ) {
+			return;
+		}
+
+		$wp_admin_bar->add_node( [
+			'id'    => 'mai-archive-page',
+			'title' => '<span class="ab-icon dashicons dashicons-admin-links"></span><span class="ab-label">' . __( 'View Archive Page', 'mai-archive-pages' ) . '</span>',
+			'href'  => $link,
+		] );
+	}
+
+	function do_content() {
+		$slug = '';
+
+		if ( $this->is_taxonomy() && ! is_paged() ) {
+
+			$term = get_queried_object();
+			$slug = $term ? $this->get_term_slug( $term->term_id ) : '';
+
+		} elseif ( $this->is_post_type() && ! is_paged() ) {
+
+			$slug = $this->get_post_type_slug( get_post_type() );
+		}
+
+		if ( ! $slug ) {
+			return;
+		}
+
+		$posts = get_posts(
+			[
+				'post_type'              => $this->post_type,
+				'post_status'            => 'any',
+				'name'                   => $slug,
+				'posts_per_page'         => 1,
+				'no_found_rows'          => true,
+				'update_post_meta_cache' => false,
+				'update_post_term_cache' => false,
+			]
+		);
+
+		if ( ! $posts ) {
+			return;
+		}
+
+		$post    = reset( $posts );
+		$content = $this->get_processed_content( $post->post_content );
+
+		if ( ! $content ) {
+			return;
+		}
+
+		genesis_markup(
+			[
+				'open'    => '<div %s>',
+				'close'   => '</div>',
+				'content' => $content,
+				'context' => 'archive-page-content',
+				'echo'    => true,
+			]
+		);
+	}
+
+	function get_processed_content( $content ) {
+		if ( function_exists( 'mai_get_processed_content' ) ) {
+			return mai_get_processed_content( $content );
+		}
+
+		/**
+		 * Embed.
+		 *
+		 * @var WP_Embed $wp_embed Embed object.
+		 */
+		global $wp_embed;
+
+		$content = $wp_embed->autoembed( $content );     // WP runs priority 8.
+		$content = $wp_embed->run_shortcode( $content ); // WP runs priority 8.
+		$content = do_blocks( $content );                // WP runs priority 9.
+		$content = wptexturize( $content );              // WP runs priority 10.
+		$content = wpautop( $content );                  // WP runs priority 10.
+		$content = shortcode_unautop( $content );        // WP runs priority 10.
+		$content = function_exists( 'wp_filter_content_tags' ) ? wp_filter_content_tags( $content ) : wp_make_content_images_responsive( $content ); // WP runs priority 10. WP 5.5 with fallback.
+		$content = do_shortcode( $content );             // WP runs priority 11.
+		$content = convert_smilies( $content );          // WP runs priority 20.
+
+		return $content;
+	}
+
+	function get_page_id() {
+		$slug = false;
+		if ( $this->is_taxonomy() ) {
+			$slug = $this->get_term_slug( get_queried_object_id() );
+		} elseif ( $this->is_post_type() ) {
+			$slug = $this->get_post_type_slug( get_post_type() );
+		}
+		return $slug ? get_page_by_path( $slug, OBJECT, $this->post_type ) : 0;
+	}
+
+	function get_post_type_title( $post_type ) {
+		$post_type = get_post_type_object( $post_type );
+		return $this->get_title( $post_type->label );
+	}
+
+	function get_post_type_slug( $post_type) {
+		return $this->get_slug( $post_type );
+	}
+
+	function get_term_title( $term_id ) {
+		$term     = get_term( $term_id );
+		$taxonomy = get_taxonomy( $term->taxonomy );
+		return $this->get_title( $term->name, $taxonomy->labels->singular_name );
+	}
+
+	function get_term_slug( $term_id ) {
+		$term = get_term( $term_id );
+		return $this->get_slug( $term->taxonomy, $term_id );
+	}
+
+	function get_title( $title, $name = '' ) {
+		$name = $name ?: __( 'Archive', 'mai-archive-pages' );
+		return sprintf( '%s [%s]', $title, $name );
+	}
+
+	function get_slug( $type, $name = '' ) {
+		return $name ? sprintf( '%s_%s_%s', $this->prefix, $type, $name ) : sprintf( '%s_%s', $this->prefix, $type );
+	}
+
+	function is_post_type() {
+		return is_home() || is_post_type_archive();
+	}
+
+	function is_taxonomy() {
+		return is_category() || is_tag() || is_tax();
+	}
+
+	function has_access() {
+		return is_user_logged_in() && current_user_can( 'edit_posts' );
+	}
+}
---
+++ mai-archive-pages.php
@@ -0,0 +1,258 @@
+<?php
+
+/**
+ * Plugin Name:     Mai Archive Pages
+ * Plugin URI:      https://bizbudding.com
+ * Description:     Build robust and SEO-friendly archive pages with blocks.
+ * Version:         0.1.0
+ *
+ * Author:          BizBudding, Mike Hemberger
+ * Author URI:      https://bizbudding.com
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Main Mai_Archive_Pages_Plugin Class.
+ *
+ * @since 0.1.0
+ */
+final class Mai_Archive_Pages_Plugin {
+
+	/**
+	 * @var   Mai_Archive_Pages_Plugin The one true Mai_Archive_Pages_Plugin
+	 * @since 0.1.0
+	 */
+	private static $instance;
+
+	/**
+	 * Main Mai_Archive_Pages_Plugin Instance.
+	 *
+	 * Insures that only one instance of Mai_Archive_Pages_Plugin exists in memory at any one
+	 * time. Also prevents needing to define globals all over the place.
+	 *
+	 * @since   0.1.0
+	 * @static  var array $instance
+	 * @uses    Mai_Archive_Pages_Plugin::setup_constants() Setup the constants needed.
+	 * @uses    Mai_Archive_Pages_Plugin::includes() Include the required files.
+	 * @uses    Mai_Archive_Pages_Plugin::hooks() Activate, deactivate, etc.
+	 * @see     Mai_Archive_Pages_Plugin()
+	 * @return  object | Mai_Archive_Pages_Plugin The one true Mai_Archive_Pages_Plugin
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) ) {
+			// Setup the setup.
+			self::$instance = new Mai_Archive_Pages_Plugin;
+			// Methods.
+			self::$instance->setup_constants();
+			self::$instance->includes();
+			self::$instance->hooks();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Throw error on object clone.
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object therefore, we don't want the object to be cloned.
+	 *
+	 * @since   0.1.0
+	 * @access  protected
+	 * @return  void
+	 */
+	public function __clone() {
+		// Cloning instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'mai-archive-pages' ), '1.0' );
+	}
+
+	/**
+	 * Disable unserializing of the class.
+	 *
+	 * @since   0.1.0
+	 * @access  protected
+	 * @return  void
+	 */
+	public function __wakeup() {
+		// Unserializing instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'mai-archive-pages' ), '1.0' );
+	}
+
+	/**
+	 * Setup plugin constants.
+	 *
+	 * @access  private
+	 * @since   0.1.0
+	 * @return  void
+	 */
+	private function setup_constants() {
+
+		// Plugin version.
+		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_VERSION' ) ) {
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_VERSION', '0.1.0' );
+		}
+
+		// Plugin Folder Path.
+		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_DIR' ) ) {
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		}
+
+		// Plugin Includes Path.
+		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_INCLUDES_DIR' ) ) {
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_INCLUDES_DIR', MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_DIR . 'includes/' );
+		}
+
+		// Plugin Folder URL.
+		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_URL' ) ) {
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		}
+
+		// Plugin Root File.
+		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_FILE' ) ) {
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_PLUGIN_FILE', __FILE__ );
+		}
+
+		// Plugin Base Name
+		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_BASENAME' ) ) {
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_BASENAME', dirname( plugin_basename( __FILE__ ) ) );
+		}
+
+	}
+
+	/**
+	 * Include required files.
+	 *
+	 * @access  private
+	 * @since   0.1.0
+	 * @return  void
+	 */
+	private function includes() {
+		// Include vendor libraries.
+		require_once __DIR__ . '/vendor/autoload.php';
+		// Includes.
+		foreach ( glob( MAI_ARCHIVE_PAGES_PLUGIN_INCLUDES_DIR . '*.php' ) as $file ) { include $file; }
+	}
+
+	/**
+	 * Run the hooks.
+	 *
+	 * @since   0.1.0
+	 * @return  void
+	 */
+	public function hooks() {
+
+		add_action( 'admin_init', array( $this, 'updater' ) );
+		add_action( 'init',       array( $this, 'register_content_types' ) );
+
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
+	}
+
+	/**
+	 * Setup the updater.
+	 *
+	 * composer require yahnis-elsts/plugin-update-checker
+	 *
+	 * @uses    https://github.com/YahnisElsts/plugin-update-checker/
+	 *
+	 * @return  void
+	 */
+	public function updater() {
+
+		// Bail if current user cannot manage plugins.
+		if ( ! current_user_can( 'install_plugins' ) ) {
+			return;
+		}
+
+		// Bail if plugin updater is not loaded.
+		if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+			return;
+		}
+
+		// Setup the updater.
+		$updater = Puc_v4_Factory::buildUpdateChecker( 'https://github.com/maithemewp/mai-archive-pages/', __FILE__, 'mai-archive-pages' );
+
+		// Maybe set github api token.
+		if ( defined( 'MAI_GITHUB_API_TOKEN' ) ) {
+			$updater->setAuthentication( MAI_GITHUB_API_TOKEN );
+		}
+	}
+
+	/**
+	 * Register content types.
+	 *
+	 * @return  void
+	 */
+	public function register_content_types() {
+
+		/***********************
+		 *  Custom Post Types  *
+		 ***********************/
+
+		register_post_type( 'mai_archive_page', array(
+			'exclude_from_search' => true,
+			'has_archive'         => false,
+			'hierarchical'        => true,
+			'labels'              => array(
+				'name'               => _x( 'Archive Pages', 'Archive Page general name',      'mai-archive' ),
+				'singular_name'      => _x( 'Archive Page', 'Archive Page singular name',      'mai-archive' ),
+				'menu_name'          => _x( 'Archive Pages', 'Archive Page admin menu',        'mai-archive' ),
+				'name_admin_bar'     => _x( 'Archive', 'Archive Page add new on admin bar',    'mai-archive' ),
+				'add_new'            => _x( 'Add New', 'Archive Page',                         'mai-archive' ),
+				'add_new_item'       => __( 'Add New Archive Page',                            'mai-archive' ),
+				'new_item'           => __( 'New Archive Page',                                'mai-archive' ),
+				'edit_item'          => __( 'Edit Archive Page',                               'mai-archive' ),
+				'view_item'          => __( 'View Archive Page',                               'mai-archive' ),
+				'all_items'          => __( 'All Archive Pages',                               'mai-archive' ),
+				'search_items'       => __( 'Search Archive Pages',                            'mai-archive' ),
+				'parent_item_colon'  => __( 'Parent Archive Pages:',                           'mai-archive' ),
+				'not_found'          => __( 'No Archive Pages found.',                         'mai-archive' ),
+				'not_found_in_trash' => __( 'No Archive Pages found in Trash.',                'mai-archive' )
+			),
+			'menu_icon'          => 'dashicons-admin-page',
+			'public'             => false,
+			'publicly_queryable' => false,
+			'show_in_menu'       => false,
+			'show_in_nav_menus'  => false,
+			'show_in_rest'       => true,
+			'show_tagcloud'      => false,
+			'show_ui'            => true,
+			'rewrite'            => false,
+			'supports'           => array( 'title', 'editor' ),
+		) );
+	}
+
+	/**
+	 * Plugin activation.
+	 *
+	 * @return  void
+	 */
+	public function activate() {
+		$this->register_content_types();
+		flush_rewrite_rules();
+	}
+
+}
+
+/**
+ * The main function for that returns Mai_Archive_Pages_Plugin
+ *
+ * The main function responsible for returning the one true Mai_Archive_Pages_Plugin
+ * Instance to functions everywhere.
+ *
+ * Use this function like you would a global variable, except without needing
+ * to declare the global.
+ *
+ * Example: <?php $plugin = Mai_Archive_Pages_Plugin(); ?>
+ *
+ * @since 0.1.0
+ *
+ * @return object|Mai_Archive_Pages_Plugin The one true Mai_Archive_Pages_Plugin Instance.
+ */
+function Mai_Archive_Pages_Plugin() {
+	return Mai_Archive_Pages_Plugin::instance();
+}
+
+// Get Mai_Archive_Pages_Plugin Running.
+Mai_Archive_Pages_Plugin();
