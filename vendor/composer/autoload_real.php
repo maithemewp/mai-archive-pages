6f6d73a8f1fc6c5fb4e4c99b4be4117899febd31
Mike Hemberger <mike@thestizmedia.com>
2021-03-26 16:31:10 -0400

    update updater

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitacdb9082b718788c5798b428edf58e08
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitacdb9082b718788c5798b428edf58e08', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitacdb9082b718788c5798b428edf58e08', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitacdb9082b718788c5798b428edf58e08::getInitializer($loader));
         } else {
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '78f8da8e2688cce5c21b94274e4b175b8d4b8088',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '78f8da8e2688cce5c21b94274e4b175b8d4b8088',
+    ),
+    'yahnis-elsts/plugin-update-checker' => 
+    array (
+      'pretty_version' => 'v4.11',
+      'version' => '4.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3155f2d3f1ca5e7ed3f25b256f020e370515af43',
+    ),
+  ),
+);
