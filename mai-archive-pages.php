26223c01ef4ac20e2f89c195a9e46c8cc334d3b0
Mike Hemberger <mike@thestizmedia.com>
2022-01-20 13:46:34 -0500

    vbump

--- mai-archive-pages.php
+++ mai-archive-pages.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Archive Pages
  * Plugin URI:      https://bizbudding.com/mai-design-pack/
  * Description:     Build robust and SEO-friendly archive pages with blocks.
- * Version:         1.2.0
+ * Version:         1.2.1
  *
  * Author:          BizBudding
  * Author URI:      https://bizbudding.com
@@ -89,7 +89,7 @@ final class Mai_Archive_Pages_Plugin {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'MAI_ARCHIVE_PAGES_PLUGIN_VERSION' ) ) {
-			define( 'MAI_ARCHIVE_PAGES_PLUGIN_VERSION', '1.2.0' );
+			define( 'MAI_ARCHIVE_PAGES_PLUGIN_VERSION', '1.2.1' );
 		}
 
 		// Plugin Folder Path.
